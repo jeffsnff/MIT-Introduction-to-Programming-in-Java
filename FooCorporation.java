5230f5b51b8d875f5525e20ac7ac9d0f2f81c010
Jeffrey Seneff <jeffsnff@gmail.com>
2024-07-17 06:36:06 -0600

    Added a check to insure basepay is greater or equal to 8 and max hours is less than or equal to 60

--- FooCorporation.java
+++ FooCorporation.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 class FooCorporation{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        double employeeTotal = 0.0;
+        double minimumWage = 8.00;
+        int maxHours = 60;
 
         System.out.print("Employee ID: ");
         String employeeID = scanner.nextLine();
@@ -11,7 +14,13 @@ class FooCorporation{
         System.out.print("Employee Hours Worked: ");
         int employeeHours = Integer.valueOf(scanner.nextLine());
 
-        System.out.println("ID "+ employeeID +" Pay "+ employeeBasepay +" Hours "+employeeHours);
+        if(employeeBasepay < minimumWage || employeeHours > maxHours){
+            System.err.println("Employee needs a minimum of $8.00 an hour and max 60 hours per pay period");
+            return;
+        }else{
+            employeeTotal = calculatepay(employeeBasepay, maxHours);
+        }
+        System.out.println("Emplyees total pay before taxes: "+employeeTotal);
 
     }
 
